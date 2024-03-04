9433b4bffeb6021512291435cf030208102f6a76
fifis83 <111370732+fifis83@users.noreply.github.com>
2024-03-04 17:10:29 +0100

    Add files via upload

---
+++ Battleship/Battleship/Coordinate.cs
@@ -0,0 +1,13 @@
+﻿namespace statki
+{
+    internal class Coordinate
+    {
+        public int row, col;
+
+        public Coordinate(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+    }
+}
